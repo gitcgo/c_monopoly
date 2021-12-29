2d205bde5206c0ecbb6fe66707b49099f0e0d604
gitcgo <92435449+gitcgo@users.noreply.github.com>
2021-12-29 16:57:44 +0100

    Update pion.cpp

--- pion.cpp
+++ pion.cpp
@@ -8,11 +8,15 @@
 #include "pion.h"
 
 pion::pion() {
-	// TODO Auto-generated constructor stub
-
+	Position = depart; //Le pion est forcément situé à la case départ lors de sa création. On include "case" dans pion.h et la case depart est fille de Case donc pas de soucis
+	nom = " Bob "; //Le Joueur est sensé donner un nom à son pion, sinon moi j'aime bien l'appeler Bob
+			// Je ne mets pas de joueur par défaut, ça n'a pas vraiment de sens. Encore une fois il faudra que les joueurs définissent à quel Joueur appartient le pion
 }
 
 pion::~pion() {
-	// TODO Auto-generated destructor stub
-}
+	// Destructeur automatique. Le pion reste utile jusqu'à la toute fin du jeu. 
+}	
 
+pion::deplacer(){
+	
+}
