ffc0cdf5ab08ad8016c3462e59b5c6223303a517
gitcgo <92435449+gitcgo@users.noreply.github.com>
2021-12-29 16:57:51 +0100

    Update pion.h

--- pion.h
+++ pion.h
@@ -17,18 +17,24 @@ using namespace std;
 
 class pion {
 	private :
-		joueur Joueur ;
-		Case Position ;
-		string nom ;
+		joueur Joueur ; //Nom du joueur associé au Pion
+		Case Position ; //Position du pion sur le plateau
+		string nom ; //Nom du pion
 	public:
-		pion();
-		virtual ~pion();
-		void setJoueur ( joueur Joueur ) {this->Joueur = Joueur;};
-		joueur getJoueur () { return this->Joueur; };
-		Case getPosition() {return this->Position;};
-		void deplacer();
-		void setNom(string nom);
-		void setPosition(Case truc) {this->Position = truc;};
+		pion(); //Constructeur du pion
+		virtual ~pion(); //Destructeur du pion, inutile s'il reste jusqu'à la fin de la partie mais on le laisse pour la beauté du geste
+	
+		void setJoueur ( joueur Joueur ) {this->Joueur = Joueur;}; //Définis quel joueur possède ce pion
+		joueur getJoueur () { return this->Joueur; }; //Return quel joueur possède ce pion
+	
+		void setPosition(Case truc) {this->Position = truc;}; //Définis la case sur laquelle se situe le pion
+		Case getPosition() {return this->Position;}; //Return la position où se trouve le pion
+	
+		void setNom(string nom){this->nom = nom}; //Définis le nom du pion
+		string getNom(){return this->nom;}; //Return le nom du Pion
+	
+		void deplacer(); //Déplace le pion sur le plateau
+		void setPosition(Case truc) {this->Position = truc;}; //Définis la case sur laquelle se situe le pion
 };
 
 #endif /* PION_H_ */
