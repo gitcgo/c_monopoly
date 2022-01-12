5c02f2e27256e0cd23ad7ba900e27725b0687977
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-12 14:07:28 +0100

    Update Terrain.cpp
    Il faut voir de ce qu'on fait de "suivante". Pas sûre de bien comprendre son intérêt.

--- Terrain.cpp
+++ Terrain.cpp
@@ -7,8 +7,15 @@
 
 #include "Terrain.h"
 
-Terrain::Terrain(string nom,int loyer , int prixAchat,string couleur, Case* suivante):propriete(nom,loyer,prixAchat,suivante) {
+Terrain::Terrain(string nom,int loyer , int prixAchat, string couleur, Case* suivante):propriete(nom,loyer,prixAchat,suivante) {
 	this->couleur = couleur ;
+	this->nom = nom;
+	this->prixAchat = prixAchat;
+	this->loyer = loyer;
+}
+
+void arreterSur(){
+	cout << "Vous êtes arrivé sur la case " << nom << "Souhaitez-vous acheter cette propriété pour "<< loyer << "E ?" << endl;
 }
 
 Terrain::~Terrain() {
