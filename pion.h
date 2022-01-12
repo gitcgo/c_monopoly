3c4610c2264940d73d1b105a143425b1bf78aaef
Hagarde <43098624+Hagarde@users.noreply.github.com>
2022-01-12 15:11:49 +0100

    rien

--- pion.h
+++ pion.h
@@ -8,10 +8,10 @@
 #ifndef PION_H_
 #define PION_H_
 
-
 #include <iostream>
-#include "joueur.h"
 #include "Case.h"
+#include "joueur.h"
+
 using namespace std;
 
 
@@ -23,16 +23,16 @@ class pion {
 	public:
 		pion(); //Constructeur du pion
 		virtual ~pion(); //Destructeur du pion, inutile s'il reste jusqu'à la fin de la partie mais on le laisse pour la beauté du geste
-	
+
 		void setJoueur ( joueur Joueur ) {this->Joueur = Joueur;}; //Définis quel joueur possède ce pion
 		joueur getJoueur () { return this->Joueur; }; //Return quel joueur possède ce pion
-	
+
 		void setPosition(Case truc) {this->Position = truc;}; //Définis la case sur laquelle se situe le pion
 		Case getPosition() {return this->Position;}; //Return la position où se trouve le pion
-	
+
 		void setNom(string nom){this->nom = nom;}; //Définis le nom du pion
 		string getNom(){return this->nom;}; //Return le nom du Pion
-	
+
 		void deplacer(); //Déplace le pion sur le plateau
 };
 
