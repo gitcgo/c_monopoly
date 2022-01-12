3d5d49d48aed7191ed13d8d5899c2ef723391bd2
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-12 16:04:12 +0100

    Update gare.cpp

--- gare.cpp
+++ gare.cpp
@@ -13,15 +13,14 @@ gare::gare(string nom,int loyer , int prixAchat,Case* suivante):propriete(nom,lo
 	this->prixAchat = prixAchat;
 }
 
-void gare::arreterSur(){
-	ptjoueur = ;// mettre le pointeur du joueur qui est tombé sur la case
+void gare::arreterSur(joueur* ptjoueur){
 	if (proprietaire =="none"){
 		string reponse
 		cout << "Vous êtes tombé sur la case " << this->nom <<" Cette propriété n'appartient à personne. Souhaitez-vous l'acheter pour "<< prixAchat << "E ?"<<endl;
 		cin >> reponse;
 		if (reponse == Oui) {
 			joueur.setSolde(joueur.getSolde - prixAchat);
-			cout<<"Vous avez acheté la propriété "<< this->nom << ". Votre solde est maintenant de " << joueur.getSolde;
+			cout<<"Vous avez acheté la propriété "<< this->nom << ". Votre solde est maintenant de " << ptjoueur.getSolde;
 		} //Il faut ajouter la propriété à la liste des propriétés du joueur.
 		else{
 			cout<< "Vous avez décidé de ne pas acheter cette propriété"<<endl;
@@ -31,7 +30,7 @@ void gare::arreterSur(){
 		this->loyer = 25* proprietaire.getGare(); //Fonction qui n'existe pas pour l'instant: elle va chercher le nombre de gare que possède un joueur
 		//Le prix est différent en fonction du nombre de gare que le propriétaire possède.
 		cout << "Le propriétaire de cette case est "<< proprietaire << " vous lui devez " << loyer<<endl;
-		joueur.setSolde(joueur.getSolde - this->loyer); //le joueur paye le locataire le prix du loyer
+		ptjoueur.setSolde(ptjoueur.getSolde - this->loyer); //le joueur paye le locataire le prix du loyer
 		proprietaire.setSolde(joueur.setSolde + this->loyer);
 	}
 }
