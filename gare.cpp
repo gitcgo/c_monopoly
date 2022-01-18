18b4776ab9c5188e3dd78709638a54f7fa9395db
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-18 22:29:34 +0100

    Update gare.cpp

--- gare.cpp
+++ gare.cpp
@@ -1,38 +1,35 @@
 /*
  * gare.cpp
  *
- *  Created on: 23 déc. 2021
+ *  Created on: 23 dÃ©c. 2021
  *      Author: aveyronvictor
  */
 #include "joueur.h"
 #include "gare.h"
 
-gare::gare(string nom,int loyer , int prixAchat,Case* suivante):propriete(nom,loyer,prixAchat,suivante) {
-	this->nom = nom;
-	this->loyer = loyer;
-	this->prixAchat = prixAchat;
+gare::gare(string nom, int* ptLoyer, int prixAchat, Case* suivante):propriete(nom,ptLoyer,prixAchat,suivante) {
+
 }
 
-void gare::arreterSur(){
-	ptjoueur = ;// mettre le pointeur du joueur qui est tombé sur la case
-	if (proprietaire =="none"){
-		string reponse
-		cout << "Vous êtes tombé sur la case " << this->nom <<" Cette propriété n'appartient à personne. Souhaitez-vous l'acheter pour "<< prixAchat << "E ?"<<endl;
+void gare::arreterSur(joueur* ptJoueur){
+	if (ptProprietaire == NULL){
+		string reponse;
+		cout << "Vous etes tombe sur la case " << this->nom <<" Cette propriete n'appartient a  personne. Souhaitez-vous l'acheter pour "<< prixAchat << "E ?"<<endl;
 		cin >> reponse;
-		if (reponse == Oui) {
-			joueur.setSolde(joueur.getSolde - prixAchat);
-			cout<<"Vous avez acheté la propriété "<< this->nom << ". Votre solde est maintenant de " << joueur.getSolde;
-		} //Il faut ajouter la propriété à la liste des propriétés du joueur.
+		if (reponse == "Oui") {
+			ptJoueur->debiter(prixAchat);
+			cout<<"Vous avez achete la propriete "<< this->nom << ". Votre solde est maintenant de " << ptJoueur->getSolde();
+		} //Il faut ajouter la propriÃ©tÃ© Ã  la liste des propriÃ©tÃ©s du joueur.
 		else{
-			cout<< "Vous avez décidé de ne pas acheter cette propriété"<<endl;
+			cout<< "Vous avez dÃ©cidÃ© de ne pas acheter cette propriÃ©tÃ©"<<endl;
 		}
 	}
 	else{
-		this->loyer = 25* proprietaire.getGare(); //Fonction qui n'existe pas pour l'instant: elle va chercher le nombre de gare que possède un joueur
-		//Le prix est différent en fonction du nombre de gare que le propriétaire possède.
-		cout << "Le propriétaire de cette case est "<< proprietaire << " vous lui devez " << loyer<<endl;
-		joueur.setSolde(joueur.getSolde - this->loyer); //le joueur paye le locataire le prix du loyer
-		proprietaire.setSolde(joueur.setSolde + this->loyer);
+		int loyer = ptLoyer[ptJoueur.getGare()-1]; //Fonction qui n'existe pas pour l'instant: elle va chercher le nombre de gare que possede un joueur. On fait -1 car ptLoyer est une liste qui commence à 0.
+		//Le prix est diffÃ©rent en fonction du nombre de gare que le propriÃ©taire possÃ¨de.
+		cout << "Le proprietaire de cette case est "<< ptProprietaire << " vous lui devez " << loyer<<endl;
+		ptJoueur->debiter(loyer); //le joueur paye le locataire le prix du loyer
+		ptProprietaire->crediter(loyer);
 	}
 }
 gare::~gare() {
