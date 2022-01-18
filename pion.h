5284b29ac13aad02a9c6ecb519d5a17e03d77d9c
Hagarde <43098624+Hagarde@users.noreply.github.com>
2022-01-18 14:24:49 +0100

    réparatipon joueur et tout

--- pion.h
+++ pion.h
@@ -14,21 +14,23 @@
 
 using namespace std;
 
+class joueur;
+class Case;
 
 class pion {
 	private :
-		joueur Joueur ; //Nom du joueur associé au Pion
-		Case Position ; //Position du pion sur le plateau
+		joueur* ptJoueur ; //Nom du joueur associé au Pion
+		Case* ptPosition ; //Position du pion sur le plateau
 		string nom ; //Nom du pion
 	public:
 		pion(); //Constructeur du pion
 		virtual ~pion(); //Destructeur du pion, inutile s'il reste jusqu'à la fin de la partie mais on le laisse pour la beauté du geste
 
-		void setJoueur ( joueur Joueur ) {this->Joueur = Joueur;}; //Définis quel joueur possède ce pion
-		joueur getJoueur () { return this->Joueur; }; //Return quel joueur possède ce pion
+		void setJoueur ( joueur* Joueur ) {this->ptJoueur = Joueur;}; //Définis quel joueur possède ce pion
+		joueur* getJoueur () { return this->ptJoueur; }; //Return quel joueur possède ce pion
 
-		void setPosition(Case truc) {this->Position = truc;}; //Définis la case sur laquelle se situe le pion
-		Case getPosition() {return this->Position;}; //Return la position où se trouve le pion
+		void setPosition(Case* truc) {this->ptPosition = truc;}; //Définis la case sur laquelle se situe le pion
+		Case* getPosition() {return this->ptPosition;}; //Return la position où se trouve le pion
 
 		void setNom(string nom){this->nom = nom;}; //Définis le nom du pion
 		string getNom(){return this->nom;}; //Return le nom du Pion
