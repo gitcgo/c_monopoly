a501d6a0a8c830aa04a5574099d7ae2bf9f57c9a
gitcgo <92435449+gitcgo@users.noreply.github.com>
2022-01-18 15:34:10 +0100

    Pointeurs pour l'objet Joueur
    Pour palier au problème de définition circulaire

--- pion.h
+++ pion.h
@@ -9,26 +9,30 @@
 #define PION_H_
 
 #include <iostream>
-#include "Case.h"
-#include "joueur.h"
+//#include "Case.h"
 
 using namespace std;
-
+class joueur; //Forward declaration
 
 class pion {
 	private :
-		joueur Joueur ;
-		Case Position ;
-		string nom ;
+		joueur* Joueur ; //Pointeur vers le Joueur qui possède le pion
+		Case *Position ; //Pointeur vers la position du pion sur le plateau
+		string nom ; //Nom du pion
 	public:
-		pion();
-		virtual ~pion();
-		void setJoueur ( joueur Joueur ) {this->Joueur = Joueur;};
-		joueur getJoueur () { return this->Joueur; };
-		Case getPosition() {return this->Position;};
-		void deplacer();
-		void setNom(string nom);
-		void setPosition(Case truc) {this->Position = truc;};
+		pion(string nom); //Constructeur du pion
+		virtual ~pion(); //Destructeur du pion, inutile s'il reste jusqu'à la fin de la partie mais on le laisse pour la beauté du geste
+
+		void setJoueur (joueur* Joueur ) {this->Joueur = Joueur;}; //Définis quel joueur possède ce pion
+		joueur* getJoueur () { return this->Joueur; }; //Return quel joueur possède ce pion
+
+		//void setPosition(Case truc) {this->Position = truc;}; //Définis l'adresse de la case sur laquelle se situe le pion
+		//Case getPosition() {return this->Position;}; //Return la position où se trouve le pion
+
+		string getNom(){return this->nom;}; //Return le nom du Pion
+
+		void deplacer(); //Déplace le pion sur le plateau
 };
+
 
 #endif /* PION_H_ */
