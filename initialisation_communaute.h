063e377349c86dee45dd52038661697bd7ed691b
royantk <killianr479@gmail.com>
2022-01-18 16:12:12 +0100

    ghioef zeh fhzefioez fhezioe zhfejz kfeznjk fezb fjkzebfjkez fezjk fhzekjf ezn fjkbefkljez fvjke fbe fjkefejzh fezkjf hzek fhez fhezjf hezkjhf iezo fhjezo hfiezuhf ze hhfejzhcjdsc jcc fhccbe f e bfejk nf e grhjzea hfuierz ghrioe jgiroe  hgre   fe nre f ezf ezf ergz rg rge rth erg

--- initialisation_communaute.h
+++ initialisation_communaute.h
@@ -18,9 +18,14 @@ Carte* initialisation_communaute(Plateau plateau) {
 	depart depart1 = depart("depart 1",NULL);
 
 	// Je crée chaque carte avec text gain perte deplacement telportation
-	pile_carte_communaute[0] = Carte("Payez le police d'assurance : 50 euros",0,50,0);
-	pile_carte_communaute[1] = Carte("Vous héritez de 100",100,0,0);
+	pile_carte_communaute[0] = Carte("Payez le police d'assuracicfjchjzvhzuohvzeujgviozehgoezgkezhoufgjeipgezncçnuer"ib rué"çur iué"r"é uriué"yr ui"éyry"éiu ry"éè ry"éiu rh"zeufiez hfuiezhjfgezuf hize foiez jfiuezhfuiezhfuez huefiez iu rhfiezgfez fezrfhezu fhuehferuf hezu fhezuof ezhu rfhze iufyuezhfiezuhvurjei htuiezh ezyrfhe zhujfzhe fzhhe zfbc ez hfuih ezfuihzeuyfhzeuh fuizeh hzeui fjiuzehf verhfze  hjfkezo jfjnce : 50 euros",0,50,0);
+	pile_carte_communaute[1] = Carte("Vous héritez de 100 euros",100,0,0);
 	pile_carte_communaute[2] = Carte("Vous êtes libéré de prison",0,0,0);
+	pile_carte_communaute[3] = Carte("Payez la note du médecin : 50 euros",0,50,0);
+	pile_carte_communaute[4] = Carte("Vous gagnez le 2nd prix de beauté, recevez 100 euros",100,0,0);
+	pile_carte_communaute[5] = Carte("Les contributions vous remboursent 20 euros",20,0,0);
+	pile_carte_communaute[6] = Carte("Recevez vos intérêts : 25 euros",25,0,0);
+
 
 	return pile_carte_communaute;
 }
