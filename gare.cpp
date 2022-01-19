9dcc9541936e2226be60d52482db0118ab7770b7
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-20 00:20:39 +0100

    Update gare.cpp

--- gare.cpp
+++ gare.cpp
@@ -1,16 +1,15 @@
 /*
  * gare.cpp
  *
- *  Created on: 23 d�c. 2021
+ *  Created on: 23 dÃ©c. 2021
  *      Author: aveyronvictor
  */
 #include "joueur.h"
 #include "gare.h"
 
-gare::gare(string nom, int* ptLoyer, int prixAchat, Case* suivante):propriete(nom,ptLoyer,prixAchat,suivante) {
-
+gare::gare(string nom, int* ptLoyer, int prixAchat, Case* suivante, joueur* ptProprietaire):propriete(nom,ptLoyer,prixAchat,suivante, ptProprietaire) {
+	this->nGare = 0;
 }
-
 
 void gare::arreterSur(joueur* ptJoueur){
 	if (ptProprietaire == NULL){
@@ -20,20 +19,18 @@ void gare::arreterSur(joueur* ptJoueur){
 		if (reponse == "Oui") {
 			ptJoueur->debiter(prixAchat);
 			cout<<"Vous avez achete la propriete "<< this->nom << ". Votre solde est maintenant de " << ptJoueur->getSolde();
+			this->ptProprietaire = ptJoueur; // On signale à la gare qu'elle à desormais un nouveau proprio
 		} //Il faut ajouter la propriÃ©tÃ© Ã  la liste des propriÃ©tÃ©s du joueur.
-
 		else{
 			cout<< "Vous avez dÃ©cidÃ© de ne pas acheter cette propriÃ©tÃ©"<<endl;
 		}
 	}
 	else{
-
 		int loyer = ptLoyer[ptJoueur.getGare()-1]; //Fonction qui n'existe pas pour l'instant: elle va chercher le nombre de gare que possede un joueur. On fait -1 car ptLoyer est une liste qui commence à 0.
 		//Le prix est diffÃ©rent en fonction du nombre de gare que le propriÃ©taire possÃ¨de.
 		cout << "Le proprietaire de cette case est "<< ptProprietaire << " vous lui devez " << loyer<<endl;
 		ptJoueur->debiter(loyer); //le joueur paye le locataire le prix du loyer
 		ptProprietaire->crediter(loyer);
-
 	}
 }
 gare::~gare() {
