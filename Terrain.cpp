0bc5de58c4ca9b6e9d84150843e247c73bb616a9
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-20 00:19:27 +0100

    Update Terrain.cpp

--- Terrain.cpp
+++ Terrain.cpp
@@ -7,31 +7,35 @@
 
 #include "Terrain.h"
 
-Terrain::Terrain(string nom,  string couleur, int prixAchat, int* ptLoyer, Case* suivante, joueur* ptProprietaire):propriete(nom, ptLoyer, prixAchat, suivante) {
-	this->couleur = couleur ;
+Terrain::Terrain(string nom, int* ptLoyer, int prixAchat, string couleur, Case* suivante, joueur* ptProprietaire):propriete(nom, ptLoyer, prixAchat, suivante, ptProprietaire) {
+	this->couleur = couleur;
 	this->nMaison = 0;
 }
 
 void Terrain::arreterSur(joueur* ptJoueur){
-	joueur Joueur = *ptJoueur; // Ne pas confondre ptJoueur et Joueur
 	if (ptProprietaire == NULL){
 		string reponse;
-		cout<< "La case : "<< this->nom << "est disponible. Son prix est de "<<this->prixAchat<<" €. Souhaitez-vous l'acheter ?"<<endl;
+		cout<< "le bien est disponible. Son prix est de "<<this->prixAchat<<". Souhaitez-vous l'acheter ?"<<endl;
 		cin>> reponse;
-
 		if (reponse == "oui") {
-			Joueur.debiter(this->prixAchat);
-			cout<<"Vous avez achete la propriete "<< this->nom << ". Votre solde est maintenant de " << Joueur.getSolde();
-			ptProprietaire = ptJoueur; // maintenant elle appartient à qqn
+			ptJoueur->debiter(prixAchat);
+			cout<<"Vous avez achete la propriete "<< this->nom << ". Votre solde est maintenant de " << ptJoueur->getSolde();
+			this->ptProprietaire = ptJoueur; //On signale à la propriete qu'il y a désormais un proprio
 		} //Il faut ajouter la propriÃ©tÃ© Ã  la liste des propriÃ©tÃ©s du joueur.
 		else{
-			cout<< "Vous avez décidé de ne pas acheter cette propriété"<<endl;
-		}
+			cout<< "Vous avez dÃ©cidÃ© de ne pas acheter cette propriÃ©tÃ©"<<endl;
+	}
 	}
 	else{
 		int loyer = ptLoyer[nMaison]; //On adapte le loyer en fonction du nombre de maison qu'il y a (ptLoyer[0]->0 maison et ptLoyer[5]->1 hotel)
-		cout << "Vous êtes tombé sur la case " << this->nom << ", vous devez payer "<< loyer << " € ."<< endl;
-		Joueur.crediter(loyer);
+		cout << "Vous etes tombe sur la case " << this->nom << ", vous devez payer "<< loyer << " E."<< endl;
+		ptProprietaire->crediter(loyer);
 		ptJoueur->debiter(loyer);
-};
+
+}
+
+
+Terrain::~Terrain() {
+	// TODO Auto-generated destructor stub
+}
 
