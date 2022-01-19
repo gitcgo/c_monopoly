14d19e7be45d88b2d83e5165cd0ebfb65df18343
NicoChap <96788691+NicoChap@users.noreply.github.com>
2022-01-20 00:16:26 +0100

    Update propriete.h

--- propriete.h
+++ propriete.h
@@ -13,16 +13,16 @@
 class propriete: public Case {
 protected :
 	string nom ;
-	int* ptLoyer ;
+	int* ptLoyer [6];
 	int prixAchat ;
 	joueur* ptProprietaire;
 public:
-	propriete(string nom, int* ptLoyer, int prixAchat, Case* suivante, joueur* ptProprietaire= NULL);
+	propriete(string nom, int* ptLoyer, int prixAchat, Case* suivante, joueur* ptProprietaire);
 	virtual ~propriete();
 	void arreterSur() ;
 	string getNom(){return this->nom;};
 	void setNom(string Nom){this->nom=Nom;};
-	int* getPtLoyer(){return this->ptLoyer;};
+	int getLoyer(){return this->ptLoyer;};
 	void setLoyer(int* ptLoyer){this->ptLoyer = ptLoyer;};
 	void setPrixAchat(int prix){this->prixAchat = prix;};
 	int getPrixAchat(){return this->prixAchat;};
