1fd0d2d9c54fb880cd32720ec1ff47e731ab498d
Hagarde <43098624+Hagarde@users.noreply.github.com>
2022-01-20 10:48:51 +0100

    Update propriete.h

--- propriete.h
+++ propriete.h
@@ -4,30 +4,26 @@
  *  Created on: 23 dÃ©c. 2021
  *      Author: aveyronvictor
  */
-
 #ifndef PROPRIETE_H_
 #define PROPRIETE_H_
-
 #include "Case.h"
-
 class propriete: public Case {
 protected :
 	string nom ;
-	int* ptLoyer [6];
+	int* ptLoyer ;
 	int prixAchat ;
 	joueur* ptProprietaire;
 public:
-	propriete(string nom, int* ptLoyer, int prixAchat, Case* suivante, joueur* ptProprietaire);
+	propriete(string nom, int* ptLoyer, int prixAchat, Case* suivante, joueur* ptProprietaire= NULL);
 	virtual ~propriete();
 	void arreterSur() ;
 	string getNom(){return this->nom;};
 	void setNom(string Nom){this->nom=Nom;};
-	int getLoyer(){return this->ptLoyer;};
+	int* getPtLoyer(){return this->ptLoyer;};
 	void setLoyer(int* ptLoyer){this->ptLoyer = ptLoyer;};
 	void setPrixAchat(int prix){this->prixAchat = prix;};
 	int getPrixAchat(){return this->prixAchat;};
 	void SetProprietaire(joueur* ptProprietaire){this->ptProprietaire = ptProprietaire;};
 	joueur* GetProprietaire(){return this->ptProprietaire;};
 };
-
 #endif /* PROPRIETE_H_ */
